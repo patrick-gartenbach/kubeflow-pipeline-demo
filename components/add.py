349adc5296229c6bc24604ef2eaa9b62ca556de0
patrick-gartenbach <patrick.gartenbach@hpe.com>
2021-07-20 17:15:23 +0200

    initial component commit

---
+++ components/add.py
@@ -0,0 +1,3 @@
+def add(a: float, b: float) -> float:
+    '''Calculates sum of two arguments'''
+    return a + b
---
+++ components/divide.py
@@ -0,0 +1,3 @@
+def divide(a: float, b: float) -> float:
+    '''Calculates sum of two arguments'''
+    return a / b
